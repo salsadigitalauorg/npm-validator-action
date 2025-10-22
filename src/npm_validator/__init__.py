b03639aee497fbd82961d5b20e363f97971a7b73
salsadeploy <deployer@salsadigital.com.au>
2025-10-22 08:47:03 +0000

    chore: mirror ab7c38fd121cc5dc5d56cad3d6ea271f2a741802 [skip ci]

---
+++ src/npm_validator/__init__.py
@@ -0,0 +1,9 @@
+"""npm-validator core package.
+
+This package provides reusable scanning logic that is callable from both the
+GitHub Action wrapper and a future standalone CLI.
+"""
+
+__all__ = [
+    "core",
+]
